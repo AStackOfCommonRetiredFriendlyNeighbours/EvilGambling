bea92714e098a69405116ccb6d1ba939bf037923
Elias <deine.email@example.com>
2026-05-28 11:26:04 +0200

    Gambling update

--- src/main/java/evilgambling/numbergen/TestForNumberGen.java
+++ src/main/java/evilgambling/numbergen/TestForNumberGen.java
@@ -6,9 +6,26 @@ public class TestForNumberGen {
 		// TODO Auto-generated method stub
 		 RandomNumberGenerators generator = new RandomNumberGenerators();
 
-	        int currentwin = generator.generate();
-
-	        System.out.println(currentwin);
+	        int reel1 = generator.generate();
+	        int reel2 = generator.generate();
+	        int reel3 = generator.generate();
+	        
+	        System.out.println("\n" + "| " + reel1 + " | " + reel2 + " | " + reel3 + " |" + "\n");
+	        
+	        if (reel1 == reel2) {
+	        		if (reel2 == reel3) {
+	        			int winnings = reel1;
+	        			if (winnings == 1) {
+	        				System.out.println("You won: " + winnings + " Grape!");
+	        			} else {
+	        				System.out.println("You won: " + winnings + " Grapes!");
+	        			}
+	        		} else {
+	        			System.out.println("You lost!");
+	        		}
+	        } else {
+	        	System.out.println("You lost!");
+	        }
 	}
 
 }
