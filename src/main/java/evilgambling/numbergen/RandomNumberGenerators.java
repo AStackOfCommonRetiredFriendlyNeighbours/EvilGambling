2d3a3e6e08d089d67b05f723b2b5d0b6273f30b3
Elias <deine.email@example.com>
2026-05-28 09:25:42 +0200

    Random winnings generator wörks now

--- src/main/java/evilgambling/numbergen/RandomNumberGenerators.java
+++ src/main/java/evilgambling/numbergen/RandomNumberGenerators.java
@@ -3,5 +3,9 @@ package evilgambling.numbergen;
 import java.util.Random;
 
 public class RandomNumberGenerators {
-	
+	private final Random random = new Random();
+
+    public int generate() {
+        return random.nextInt(69) + 1;
+    }
 }
--- src/main/java/evilgambling/numbergen/TestForNumberGen.java
+++ src/main/java/evilgambling/numbergen/TestForNumberGen.java
@@ -4,7 +4,11 @@ public class TestForNumberGen {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 RandomNumberGenerators generator = new RandomNumberGenerators();
 
+	        int currentwin = generator.generate();
+
+	        System.out.println(currentwin);
 	}
 
 }
