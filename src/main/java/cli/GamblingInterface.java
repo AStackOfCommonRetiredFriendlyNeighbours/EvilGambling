cea4284cf630afd93b95e919603a6ca250f7f143
Elias <deine.email@example.com>
2026-06-02 14:24:18 +0200

    It is finally evil

--- src/main/java/cli/GamblingInterface.java
+++ src/main/java/cli/GamblingInterface.java
@@ -1,16 +1,63 @@
 package cli;
 
+import evilgambling.numbergen.RandomNumberGenerators;
+
+import java.util.Scanner;
+
 public class GamblingInterface {
 
     public static void main(String[] args) {
-        int fakereel1 = 0;
-        int fakereel2 = 0;
-        int fakereel3 = 0;
-        int winnings = 0;
-
-        printSlotMachine(fakereel1, fakereel2, fakereel3, winnings);
+    		RandomNumberGenerators generator = new RandomNumberGenerators(1,9);
+    		
+    		RandomNumberGenerators spins = new RandomNumberGenerators(10, 1000);
+    		
+    		Scanner scanner = new Scanner(System.in);
+    		
+    		//int winnings is going to be 0 every time you run this code we need to rethink for GUI
+    		
+    		int winnings = 0;
+    		
+    		int numberofspins = 0;
+    		
+    		//Warning Line 20 to 27 Is useless for GUI. DO NOT IMPLEMENT!!!!!!!!
+    		
+    		System.out.println(
+    	            "\n\t\tEvil Gambling\n\n" +
+    	            "\t\t_____________________________________\n" +
+    	            "\t\t|           |           |           |\n" +
+    	            "\t\t|     ?     |     ?     |     ?     |\n" +
+    	            "\t\t|___________|___________|___________|\n\n" +
+    	            "\t\tTotal Winnings: ?"
+    	        );
+    		
+    		while (true) {
+    			System.out.println("Press enter to spin...");
+    		    scanner.nextLine();
+    		    int fakereel1 = generator.generate();
+    		    int fakereel2 = generator.generate();
+    		    int fakereel3 = generator.generate();
+    		    int maxspins = spins.generate();
+    		    
+    		    if (fakereel1 == fakereel2) {
+    	        		if (fakereel2 == fakereel3) {
+    	        			winnings = winnings + fakereel3;
+    	        		}
+    	        }
+    		  
+    		    printSlotMachine(fakereel1, fakereel2, fakereel3, winnings);
+    		    
+    		    numberofspins ++;
+    		    
+    		    if (numberofspins > maxspins) {
+    		    		numberofspins = 0;
+    		    		winnings = 0;
+    		    }
+    		    
+    		}
     }
-
+    
+    //Attention line 50 to 59 is also useless for GUI. DO NOT IMPLEMENT!!!!!!!!!
+    
     public static void printSlotMachine(int fakereel1, int fakereel2, int fakereel3, int winnings) {
         System.out.println(
             "\n\t\tEvil Gambling\n\n" +
--- src/main/java/evilgambling/numbergen/RandomNumberGenerators.java
+++ src/main/java/evilgambling/numbergen/RandomNumberGenerators.java
@@ -3,9 +3,16 @@ package evilgambling.numbergen;
 import java.util.Random;
 
 public class RandomNumberGenerators {
-	private final Random random = new Random();
-
+	private Random random;
+	private int from;
+	private int to;
+	
+	public RandomNumberGenerators(int from, int to) {
+		random = new Random();
+		this.from = from;
+		this.to = to+1;
+	}
     public int generate() {
-    	return random.nextInt(9) + 1;
+    	return random.nextInt(from,to);
     }
 }
